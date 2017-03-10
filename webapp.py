c3c6489a5d5f64b42e3f50d38776a7c617460783
JEduardoMaldonado <joseeduardocruzmaldonado@gmail.com>
2017-03-10 09:32:31 -0600

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,10 +1,10 @@
 import web
 # @
 from web import form
-db = web.database(dbn='mysql', host='ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',db='aw10o7i0uoowomzw', user='bt9w0v9g0v8uz4x3', pw='ct31cqvwybrmkqku')
+db = web.database(dbn='mysql', host='y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',db='b1i85x9we2kb7h3f', user='vh57xlv2tj1r8ovf', pw='clsbdiom4efdeqwe',port="3360")
 render=web.template.render('templates')
 urls = (
-   '/','index',
+    '/','index',
     '/nuevo', 'nuevo',
     '/editar/(.+)','editar',
     '/ver/(.+)','ver',
@@ -13,84 +13,92 @@ urls = (
 
 
 
-myformAgenda=form.Form(
-    form.Textbox('Nombre'), 
-    form.Textbox('Apellido'),
-    form.Textbox('Telefono'),
-    form.Textbox('Correo')
+myformBorrego=form.Form(
+    form.Textbox('id_borrego'), 
+    form.Textbox('id_raza'),
+    form.Textbox('id_genetica'),
+    form.Textbox('genero'),
+    form.Textbox('edad'),
+    form.Textbox('fecha_nacimiento'),
+    form.Textbox('precio')
+    
    
 )
+
 class index:
     def GET(self):
         
-        result=db.select('datosagenda')
+        result=db.select('Borrego')
         return render.index(result)
     def POST(self):           
         raise web.seeother("/nuevo")    
 class nuevo:
     def GET(self):
-        formNew=myformAgenda()
+        formNew=myformBorrego()
         return render.nuevo(formNew)
     def POST(self): 
-        formNew = myformAgenda()
+        formNew = myformBorrego()
         if not formNew.validates(): 
             return render.nuevo(formNew)
         else:
-            db.insert('datosagenda', 
-            nombre=formNew.d.Nombre, 
-            apellido=formNew.d.Apellido, 
-            telefono=formNew.d.Telefono,
-             correo=formNew.d.Correo)
-            result=db.select('datosagenda')
-            raise web.seeother('/')
+            db.insert('Borrego', 
+            id_raza=formNew.d.id_raza, 
+            id_genetica=formNew.d.id_genetica, 
+            genero=formNew.d.genero,
+             edad=formNew.d.edad,
+             fecha_nacimiento=formNew.d.fecha_nacimiento,
+             precio=formNew.d.precio
+             )
+            result=db.select('Borrego')
+            raise web.seeother('/index')
             
 
 class editar:
     def GET(self,id):
-        formEdit=myformAgenda()
+        formEdit=myformBorrego()
         
         
-        result=db.select('datosagenda', where= "id=%s"%(id))
+        result=db.select('Borrego', where= "id=%s"%(id))
         
         for row in result:
-            formEdit['Nombre'].value=row.nombre
-            formEdit['Apellido'].value=row.apellido
-            formEdit['Telefono'].value=row.telefono
-            formEdit['Correo'].value=row.correo
+            formEdit['genero'].value=row.genero
+            formEdit['edad'].value=row.edad
+            formEdit['fecha_nacimiento'].value=row.fecha_nacimiento
+            formEdit['precio'].value=row.precio
         return render.editar(formEdit)        
     def POST(self,id):
-        formEdit=myformAgenda()
+        formEdit=myformBorrego()
         if not formEdit.validates(): 
             return render.editar(formEdit)
         else:
-            db.update('datosagenda', where='id=%s'%(id), nombre=formEdit.d.Nombre,
-             apellido=formEdit.d.Apellido, telefono=formEdit.d.Telefono,
-              correo=formEdit.d.Correo)
-            result=db.select('datosagenda')
-            raise web.seeother('/')
+            db.update('Borrego', where='id=%s'%(id), genero=formEdit.d.genero,
+             edad=formEdit.d.edad, fecha_nacimiento=formEdit.d.fecha_nacimiento,
+              precio=formEdit.d.precio)
+            result=db.select('Borrego')
+            raise web.seeother('/index')
 class eliminar:
     def GET(self,id):
-        formEdit=myformAgenda()
+        formEdit=myformBorrego()
         
-        result=db.select('datosagenda', where='id=%s'%(id))
+        result=db.select('Borrego', where='id=%s'%(id))
         for row in result:
-            formEdit['Nombre'].value=row.nombre
-            formEdit['Apellido'].value=row.apellido
-            formEdit['Telefono'].value=row.telefono
-            formEdit['Correo'].value=row.correo
+            formEdit['genero'].value=row.genero
+            formEdit['edad'].value=row.edad
+            formEdit['fecha_nacimiento'].value=row.fecha_nacimiento
+            formEdit['precio'].value=row.precio
         
         return render.eliminar(formEdit)        
     def POST(self,id):
-        formEdit=myformAgenda()
+        formEdit=myformBorrego()
         if not formEdit.validates(): 
             return render.eliminar(formEdit)
         else:
-            db.delete('datosagenda', where="id=%s"%(id))
-            raise web.seeother('/')
+            db.delete('Borrego', where="id=%s"%(id))
+            raise web.seeother('/index')
 class ver:
     def GET(self,id):
         
-        result=db.select('datosagenda', where="id=%s"%(id))
+        result=db.select('Borrego', where="id=%s"%(id))
         return render.ver(result)
 
 if __name__ == "__main__":
